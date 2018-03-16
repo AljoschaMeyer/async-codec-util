e41162bbd06edfd2b5a72ea86d2849f6ebc0ef1d
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-03-16 14:29:36 +0100

    Add testing module

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 extern crate async_codec;
 extern crate futures_core;
 extern crate futures_io;
+extern crate futures_executor;
+extern crate futures_util;
 
 #[cfg(test)]
 extern crate async_ringbuffer;
@@ -18,6 +20,7 @@ mod decoder;
 pub use decoder::Decoder;
 mod encoder;
 pub use encoder::Encoder;
+pub mod testing;
 
 /// Decode a value from an `AsyncRead`, using an `AsyncDecode`.
 pub fn decode<R, D>(reader: R, dec: D) -> decoder::Decoder<R, D> {
@@ -30,4 +33,3 @@ pub fn encode<W, C>(writer: W, co: C) -> encoder::Encoder<W, C> {
 }
 
 // TODO add Chain for chaining two encoders sequentially
-// TODO add remaining_bytes to encoder future when wrapping an AsyncEncodeLen
